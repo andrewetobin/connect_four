62b068b09390202b7f43782cd916107dc70da80a
andrewetobin <andrewetobin@gmail.com>
2018-08-09 19:36:02 -0600

    refactored board

--- lib/board.rb
+++ lib/board.rb
@@ -19,12 +19,13 @@ class Board
 
   def display_board
     board = @board_arrays
-    puts "\n\n      ABCDEFG
-      #{board[0][0].state}#{board[0][1].state}#{board[0][2].state}#{board[0][3].state}#{board[0][4].state}#{board[0][5].state}#{board[0][6].state}
-      #{board[1][0].state}#{board[1][1].state}#{board[1][2].state}#{board[1][3].state}#{board[1][4].state}#{board[1][5].state}#{board[1][6].state}
-      #{board[2][0].state}#{board[2][1].state}#{board[2][2].state}#{board[2][3].state}#{board[2][4].state}#{board[2][5].state}#{board[2][6].state}
-      #{board[3][0].state}#{board[3][1].state}#{board[3][2].state}#{board[3][3].state}#{board[3][4].state}#{board[3][5].state}#{board[3][6].state}
-      #{board[4][0].state}#{board[4][1].state}#{board[4][2].state}#{board[4][3].state}#{board[4][4].state}#{board[4][5].state}#{board[4][6].state}
-      #{board[5][0].state}#{board[5][1].state}#{board[5][2].state}#{board[5][3].state}#{board[5][4].state}#{board[5][5].state}#{board[5][6].state}   \n\n\n"
+    puts "\n\n      A   B   C   D   E   F   G
+    | #{board[0][0].state} | #{board[0][1].state} | #{board[0][2].state} | #{board[0][3].state} | #{board[0][4].state} | #{board[0][5].state} | #{board[0][6].state} |
+    | #{board[1][0].state} | #{board[1][1].state} | #{board[1][2].state} | #{board[1][3].state} | #{board[1][4].state} | #{board[1][5].state} | #{board[1][6].state} |
+    | #{board[2][0].state} | #{board[2][1].state} | #{board[2][2].state} | #{board[2][3].state} | #{board[2][4].state} | #{board[2][5].state} | #{board[2][6].state} |
+    | #{board[3][0].state} | #{board[3][1].state} | #{board[3][2].state} | #{board[3][3].state} | #{board[3][4].state} | #{board[3][5].state} | #{board[3][6].state} |
+    | #{board[4][0].state} | #{board[4][1].state} | #{board[4][2].state} | #{board[4][3].state} | #{board[4][4].state} | #{board[4][5].state} | #{board[4][6].state} |
+    | #{board[5][0].state} | #{board[5][1].state} | #{board[5][2].state} | #{board[5][3].state} | #{board[5][4].state} | #{board[5][5].state} | #{board[5][6].state} |
+    -----------------------------\n\n\n"
   end
 end
